cca36148cf387e3723748e83c4c825261b47414d
GriPet12 <pgrigorcuk0@gmail.com>
2025-10-22 23:29:28 +0300

    Added basic project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SitesModule } from './sites/sites.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, SitesModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
